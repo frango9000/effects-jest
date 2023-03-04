edfec68f645403195a8fd86189ae8e1b2d5de6a2
Francisco Javier Sanchez Temprano <frango9000@gmail.com>
2023-03-04 21:25:09 +0100

    Initial commit

---
+++ apps/effects-jest/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'effects-jest-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'effects-jest';
+}
